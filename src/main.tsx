196e3c75fce3f636d1b2fb521fbe27a286412a23
Knightrider85 <daniel@grevensgastro.se>
2023-12-04 15:34:42 +0100

    remove wrong library and navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App.tsx';
-import Navbar from './Components/Navbar.tsx';
 import Bookmark from './Pages/Bookmark.tsx';
 import Cards from './Pages/Cards.tsx';
 import Categori from './Pages/Categori.tsx';
@@ -11,8 +9,6 @@ import Categori from './Pages/Categori.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-    <CssBaseline />
-    <Navbar />
       <Routes>
       <Route path="/" element={<App />} />
         <Route path="/categori" element={<Categori />} />
