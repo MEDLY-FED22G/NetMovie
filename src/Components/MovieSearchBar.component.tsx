8a26e5484b7114333f1097156360cc632d1ff6d9
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-12-05 15:05:13 +0100

    Suggested change: Remove FA icon and change to existing icon library(tabler)

--- src/Components/MovieSearchBar.component.tsx
+++ src/Components/MovieSearchBar.component.tsx
@@ -1,54 +1,50 @@
-import { useState } from 'react';
 import { Autocomplete, Button, Container } from '@mantine/core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { IconSearch } from '@tabler/icons-react';
+import { useState } from 'react';
 
 const MovieSearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
   const autocompleteStyles = {
     input: {
-      fontWeight: 400, 
-      fontSize: 14, 
+      fontWeight: 400,
+      fontSize: 14,
       color: '#1A1B1E',
       border: 'none',
       borderRadius: '4px',
       backgroundColor: '#F0F0F0',
 
       '&:focus': {
-        borderColor: '#8C7AE6', 
+        borderColor: '#8C7AE6',
       },
-      '&:hover': {
-      }
+      '&:hover': {},
     },
   };
 
   return (
     <Container>
-        <Autocomplete
+      <Autocomplete
         value={searchTerm}
         onChange={setSearchTerm}
         placeholder="Search for a movie..."
         styles={autocompleteStyles}
         rightSection={
-            <Button
-          onClick={() => {
-            console.log('Searching for:', searchTerm);
-          }}
-          variant="subtle"
-          style={{
-            backgroundColor: 'transparent',
-            padding: 0,
-          }}
-        >
-                <FontAwesomeIcon icon={faSearch} color='black'/>
-            </Button>
-          }
-          rightSectionWidth={60}
-        />
+          <Button
+            onClick={() => {
+              console.log('Searching for:', searchTerm);
+            }}
+            variant="subtle"
+            style={{
+              backgroundColor: 'transparent',
+              padding: 0,
+            }}
+          >
+            <IconSearch color="black" />
+          </Button>
+        }
+        rightSectionWidth={60}
+      />
     </Container>
-   
   );
 };
 
