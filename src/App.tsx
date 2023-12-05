e59182dfa3b38b61204aee7fc0d5c91fc083066d
Emil Helgesson <helgesson.emil.94@gmail.com>
2023-12-05 19:47:29 +0100

    Minor fix: Add capital letter to import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import { Footer } from './components/Footer';
-import Header from './components/Header';
+import { Footer } from './Components/Footer';
+import Header from './Components/Header';
 
 function App() {
   return (
