07ffe957653a80435f95b13fb2c5def07743ba9d
Emil Helgesson <114802306+Emil-Helge@users.noreply.github.com>
2023-12-06 13:38:51 +0100

    Install and setup to enable testing (#36)
    * install dependencies
    
    * setup to enable testing
    
    * Adjust import path to new location(context)
    
    * Add test to verify that year and rating renders

---
+++ src/tests/setup.tsx
@@ -0,0 +1,33 @@
+import { MantineProvider } from '@mantine/core';
+import '@testing-library/jest-dom/vitest';
+import { RenderOptions, cleanup, render } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { afterEach, beforeAll, vi } from 'vitest';
+
+// Före alla tester, simulera window.matchMedia-funktionen för att undvika fel vid körning av tester
+beforeAll(() => {
+  window.matchMedia = vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }));
+});
+
+// Definiera en anpassad render-funktion som omsluter komponenten med nödvändiga context-leverantörer
+export const customRender = (ui: ReactElement, options?: RenderOptions) =>
+  render(
+    <Router>
+      <MantineProvider>{ui}</MantineProvider>
+    </Router>,
+    options,
+  );
+
+afterEach(() => {
+  cleanup();
+});
