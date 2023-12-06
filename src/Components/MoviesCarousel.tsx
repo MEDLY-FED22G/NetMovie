c0d1981ca17f31bbdb3f82c136ecfd007e526d03
Knightrider85 <daniel@grevensgastro.se>
2023-12-06 14:10:53 +0100

    mo_image vill inte funka men bookmark finns

--- src/Components/MoviesCarousel.tsx
+++ src/Components/MoviesCarousel.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from 'react';
 import moviesData from '../data/movies.json';
 import { Movie } from '../Components/MovieContext';
 import { IconArrowBadgeLeft, IconArrowBadgeRight } from '@tabler/icons-react';
+import BookmarkButton from './BookmarkButton';
 
-
-//ny pr
 export function MoviesCarousel() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -18,15 +17,33 @@ export function MoviesCarousel() {
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + movies.length) % movies.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + movies.length) % movies.length,
+    );
   };
+
 
   return (
     <div style={{ position: 'relative', overflow: 'hidden', width: '100%' }}>
-      <button onClick={handlePrev} style={{ position: 'absolute', left: '0', top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+      <button
+        onClick={handlePrev}
+        style={{
+          position: 'absolute',
+          left: '0',
+          top: '50%',
+          transform: 'translateY(-50%)',
+          zIndex: 1,
+        }}
+      >
         <IconArrowBadgeLeft />
       </button>
-      <div style={{ display: 'flex', transition: 'transform 0.5s ease-in-out', marginLeft: `-${currentIndex * (400 + 20)}px` }}>
+      <div
+        style={{
+          display: 'flex',
+          transition: 'transform 0.5s ease-in-out',
+          marginLeft: `-${currentIndex * (400 + 20)}px`,
+        }}
+      >
         {movies.map((movie) => (
           <div
             key={movie.title}
@@ -38,27 +55,72 @@ export function MoviesCarousel() {
               display: 'flex',
               flexDirection: 'column',
               justifyContent: 'space-between',
-              backgroundImage: `url(${movie.thumbnail})`,
+              backgroundImage: `url(${movie.thumbnail ? movie.thumbnail : '/src/assets/no_image.png'})`,
               backgroundSize: 'cover',
               backgroundPosition: 'center',
               margin: '10px',
-              position: 'relative', 
+              position: 'relative',
             }}
           >
-            <div style={{ fontWeight: 700, textTransform: 'uppercase', opacity: 0.7, position: 'absolute', top: '10px', left: '10px' }}>
+            <div
+              style={{
+                position: 'absolute',
+                top: '10px',
+                right: '10px',
+                zIndex: 2,
+              }}
+            >
+              <BookmarkButton />
+            </div>
+            <div
+              style={{
+                fontWeight: 700,
+                textTransform: 'uppercase',
+                opacity: 0.7,
+                position: 'absolute',
+                top: '10px',
+                left: '10px',
+              }}
+            >
               {movie.genre}
             </div>
-            <div style={{ fontWeight: 900, fontSize: '1.5rem', marginBottom: '8px', position: 'absolute', bottom: '10px', left: '10px' }}>
+            <div
+              style={{
+                fontWeight: 900,
+                fontSize: '1.5rem',
+                marginBottom: '8px',
+                position: 'absolute',
+                bottom: '10px',
+                left: '10px',
+              }}
+            >
               {movie.title}
             </div>
-            <div style={{ fontWeight: 700, opacity: 0.7, position: 'absolute', bottom: '10px', right: '10px' }}>{movie.year}</div>
+            <div
+              style={{
+                fontWeight: 700,
+                opacity: 0.7,
+                position: 'absolute',
+                bottom: '10px',
+                right: '10px',
+              }}
+            >
+              {movie.year}
+            </div>
           </div>
         ))}
       </div>
-      <button onClick={handleNext} style={{ position: 'absolute', right: '0', top: '50%', transform: 'translateY(-50%)' }}>
+      <button
+        onClick={handleNext}
+        style={{
+          position: 'absolute',
+          right: '0',
+          top: '50%',
+          transform: 'translateY(-50%)',
+        }}
+      >
         <IconArrowBadgeRight />
       </button>
     </div>
   );
 }
-
