fe1430c2d237a739b0b94bf92b3e4766fe575b3b
Knightrider85 <daniel@grevensgastro.se>
2023-12-06 12:14:23 +0100

    carousel clean up files not needed

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,15 +4,14 @@ export default function Home() {
   const containerStyle: React.CSSProperties = {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center', // Center content horizontally
-    marginTop: '50px', // Adjust margin as needed
+    alignItems: 'center', 
+    marginTop: '50px',
     padding: '20px'
   };
 
   return (
     <div style={containerStyle}>
       <MoviesCarousel />
-      {/* <CardsCarousel /> */}
     </div>
   );
 }
