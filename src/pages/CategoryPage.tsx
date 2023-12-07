4b4e978445ec54fa6a3c6e2c15ca26163fc1dc0a
ye-mou <git.ymo22@gmail.com>
2023-12-07 20:11:53 +0100

    Add handlecategory change Filtered movies variable to filter on category

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,14 +1,44 @@
-import { Container } from '@mantine/core';
+import { Container, Tabs } from '@mantine/core';
 import React, { useState } from 'react';
+import MovieCard from '../components/MovieCard';
+import { useMovieContext } from '../components/MovieContext';
 
 const CategoryPage: React.FC = () => {
-  const [selectedCategory, setSelectedCategory] = useState<string>();
+  const { movies } = useMovieContext();
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const categories = Array.from(new Set(movies.map((movie) => movie.genre)));
+
+  const handleCategoryChange = (category: string | null) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredMovies = selectedCategory
+    ? movies.filter((movie) => movie.genre === selectedCategory)
+    : movies;
 
   return (
     <Container size="xl" py={30} mih="calc(100vh - 129px)">
       <h2>Movie Categories</h2>
 
-      <div style={{ display: 'flex' }}></div>
+      <Tabs defaultValue={categories[0]} onChange={handleCategoryChange}>
+        <Tabs.List>
+          {categories.map((category) => (
+            <Tabs.Tab key={category} value={category}>
+              {category}
+            </Tabs.Tab>
+          ))}
+        </Tabs.List>
+      </Tabs>
+
+      <h2>Selected Category: {selectedCategory || 'All Movies'}</h2>
+
+      {/* Display movies based on the selected category */}
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.title} {...movie} />
+        ))}
+      </div>
     </Container>
   );
 };
