657652cb44350511c94ffbe63c0f1728c627496d
Mimmi Collin <113519976+MimPMC@users.noreply.github.com>
2023-12-08 14:52:47 +0100

    53 display bookmarked movies on bookmark page (#62)
    * Bookmark logic, add movies
    
    * Update BookmarkPage.tsx

--- src/pages/BookmarkPage.tsx
+++ src/pages/BookmarkPage.tsx
@@ -1,12 +1,24 @@
-import { Container } from '@mantine/core';
+import { Container, Title, Group } from '@mantine/core';
 import React from 'react';
+import { useMovieContext } from '../components/MovieContext';
+import MovieCard from '../components/MovieCard';
+
 
 const BookmarkPage: React.FC = () => {
+  const { bookmarkedMovies } = useMovieContext();
+  
   return (
     <Container size={'xl'} py={30} mih={'calc(100vh - 129px)'}>
-      Bookmark
+      <Title mb={20} order={2}>My Bookmarks</Title>
+      <Group gap="lg">
+      {bookmarkedMovies.map((movie, index) => (
+        <MovieCard key={index} {...movie} />
+      ))}
+      </Group>
+      
     </Container>
   );
 };
+ 
 
 export default BookmarkPage;
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -9,39 +9,48 @@ import {
   Text,
   Title,
 } from '@mantine/core';
-import React, { useState } from 'react';
+import React, {  } from 'react';
 import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
 import { useMovieContext } from '../components/MovieContext';
+
 import MovieDetailsBookmarkButton from '../components/MovieDetailsBookmarkButton';
 
 const MoviePage: React.FC = () => {
-  const { movies } = useMovieContext();
+  const { movies, addBookmarkedMovie, removeBookmarkedMovie, isMovieBookmarked } = useMovieContext();
   const { title } = useParams<{ title: string }>();
-  const [isBookmarked, setIsBookmarked] = useState(false);
-
   const movie = movies.find((movie) => movie.title === title);
 
   if (!movie) {
     // Handle the case where movie details couldn't be fetched
     return <div>Error fetching movie details</div>;
   }
+  const isBookmarked = isMovieBookmarked(movie.title);
+
 
   const handleBookmarkClick = () => {
-    setIsBookmarked((prev) => !prev);
+    // Toggle bookmark status
+    if (!isBookmarked) {
+      addBookmarkedMovie(movie);
+    } else {
+      removeBookmarkedMovie(movie);
+    }
   };
+
   return (
     <Container
       size="xl"
-      pt={{ base: 10, xs: 50, md: 70 }}
+      pt={{ base: 10, xs: 30, md: 30 }}
       mih={'calc(100vh - 129px)'}
       pb={30}
     >
-      <Stack align="start" gap={15}>
+      <Stack align="center" justify='center' gap={5} h={"100%"}>
+        <Group w={"100%"}>
         <BackButton />
+        </Group>
         <Flex
           w="100%"
-          h={{ base: 'auto', xs: 400, sm: 450, md: 550 }}
+          h={{ base: 'auto', xs: 400, sm: 400, md: 450 }}
           justify="space-between"
           direction={{ base: 'column-reverse', xs: 'row' }}
           gap={{ base: 15, xs: 5 }}
