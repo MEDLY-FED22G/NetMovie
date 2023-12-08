321c3baabe2e43e6597bddbb302c545198b5916e
Linus Hammarberg <117081316+Lvl1Pichu@users.noreply.github.com>
2023-12-08 15:18:25 +0100

    56-display-all-movies-on-frontpage-and-filter-then-when-searching (#61)
    * Created MovieGrid
    
    Created a grid component that contains filteredMovies and displays them.
    Displays a text saying " no movies found" if filteredMovies are empty.
    
    * Fixed responsivity by switching Grid to Group

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { Container } from '@mantine/core';
 import MyCarousel from '../components/Carousel'; // Updated import
 import { useMovieContext } from '../components/MovieContext';
 import MovieSearchBar from '../components/MovieSearchBar.component';
-import moviesData from '../data/movies.json';
+import MoviesGrid from '../components/FilteredMovies.component';
 
 export default function Home() {
   const { trendingMovies, recommendedMovies } = useMovieContext();
@@ -10,6 +10,7 @@ export default function Home() {
   return (
     <Container size={'xl'} py={30} mih={'calc(100vh - 129px)'}>
       <MovieSearchBar />
+      <MoviesGrid/>
       <MyCarousel movies={trendingMovies} title="Trending" />
       <MyCarousel movies={recommendedMovies} title="Recommended for you"/>
     </Container>
