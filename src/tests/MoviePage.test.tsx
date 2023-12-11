5b939b1252bd1c306a9e35fec9f69869de42cedf
Linus Hammarberg <Linus.hammarberg@gmail.com>
2023-12-11 14:13:15 +0100

    Made MoviePage test

--- src/tests/MoviePage.test.tsx
+++ src/tests/MoviePage.test.tsx
@@ -1,19 +1,18 @@
-import { screen } from '@testing-library/react';
-import { expect, test } from 'vitest';
-import MoviePage from '../pages/MoviePage';
+import { fireEvent, screen } from '@testing-library/react';
+import { expect, test, describe } from 'vitest';
 import { customRender } from './setup';
+import data from '../data/movies.json';
+import MovieCard from '../components/MovieCard';
+import MoviePage from '../pages/MoviePage';
 
-test.only('MoviePage loads with the correct title and displays movie details', () => {
-  customRender(<MoviePage />);
+describe('MoviePage', () => {
+  test('renders a movie that the user Clicked on in the Homepage.', () => {
+    const movie = data[0];
+    customRender(<MovieCard {...movie} />);
+    fireEvent.click(screen.getByRole('link'));
+    customRender(<MoviePage />);
+    const movieCards = screen.getAllByAltText('Harry Potter and the Chamber of Secrets Poster');
+    expect(movieCards.length).toBeGreaterThan(1);
+  });
 
-  // Use a regular expression to allow for flexible matching
-  const titleText = screen.getByText('Harry Potter and the Chamber of Secrets', {exact : false}) ;
-  expect(titleText).toBeInTheDocument();
-
-  // Continue with the rest of your tests
-  const yearText = screen.getByText('2002');
-  expect(yearText).toBeInTheDocument();
-
-  const ratingText = screen.getByText('Rating: PG');
-  expect(ratingText).toBeInTheDocument();
 });
