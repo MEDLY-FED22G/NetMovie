724e3da8e2264f0bd4fe2f8146683dc256fea99c
Yehad <118016890+ye-mou@users.noreply.github.com>
2023-12-11 12:12:13 +0100

    49 populate the category page (#54)
    * Add state for Category
    
    * Add handlecategory change
    Filtered movies variable to filter on category
    
    * Add show all movies Logic for filteredMovies
    
    * Change to display category
    Movie that belong to that category
    
    * Small bug fix
    
    * Change to show and map genre
    
    * Wrap MovieProvider in customRender function
    
    * Mock ReSizeObserver in test setup
    
    * Update year and rating props to match changes in json-file
    
    ---------
    
    Co-authored-by: Emil Helgesson <helgesson.emil.94@gmail.com>

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,10 +1,50 @@
-import { Container } from '@mantine/core';
-import React from 'react';
+import { Container, Group, Tabs } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../components/MovieCard';
+import { useMovieContext } from '../components/MovieContext';
 
 const CategoryPage: React.FC = () => {
+  const { movies } = useMovieContext();
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  // Get all genres from movies in the data file
+  const genres = Array.from(
+    new Set(movies.flatMap((movie) => movie.genre.split(', '))),
+  );
+
+  const handleCategoryChange = (category: string | null) => {
+    setSelectedCategory(category);
+  };
+
+  // If a category is selected, filter movies that belong to that category
+  const filteredMovies = selectedCategory
+    ? movies.filter((movie) => movie.genre.includes(selectedCategory))
+    : movies;
+
+  useEffect(() => {
+    setSelectedCategory(null);
+  }, []);
+
   return (
-    <Container size={'xl'} py={30} mih={'calc(100vh - 129px)'}>
-      Category
+    <Container size="xl" py={30} mih="calc(100vh - 129px)">
+      <Tabs onChange={handleCategoryChange}>
+        <Tabs.List mb={30}>
+          {genres.map((category) => (
+            <Tabs.Tab key={category} value={category}>
+              {category}
+            </Tabs.Tab>
+          ))}
+        </Tabs.List>
+      </Tabs>
+
+      {/* <h2>{selectedCategory}</h2> */}
+
+      {/* When a category is chosen, show movies belonging to that category */}
+      <Group>
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.title} {...movie} />
+        ))}
+      </Group>
     </Container>
   );
 };
