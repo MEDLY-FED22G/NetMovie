3ff912fe84473271826f7472194569a5e5a07c1c
ye-mou <git.ymo22@gmail.com>
2023-12-11 13:49:30 +0100

    Add category tab data-test-id

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -30,7 +30,11 @@ const CategoryPage: React.FC = () => {
       <Tabs onChange={handleCategoryChange}>
         <Tabs.List mb={30}>
           {genres.map((category) => (
-            <Tabs.Tab key={category} value={category}>
+            <Tabs.Tab
+              key={category}
+              value={category}
+              data-testid={`${category}-tab`}
+            >
               {category}
             </Tabs.Tab>
           ))}
